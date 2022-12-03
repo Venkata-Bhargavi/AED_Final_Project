ba0354b69d74da0762ef9ad4b97d58f6f967dea4
KrishnakanthJ <jarapala.k@northeastern.edu>
2022-12-03 00:58:00 -0500

    Created Login Page and added all the backup

--- VaccineManagementSystem/src/Business/Role/Role.java
+++ VaccineManagementSystem/src/Business/Role/Role.java
@@ -45,7 +45,7 @@ public abstract class Role {
         }
     }
     
-    public abstract JPanel createWorkArea(JPanel userProcessContainer, 
+    public abstract JPanel createWorkArea( 
             UserAccount account, 
             Organization organization, 
             Enterprise enterprise, 
